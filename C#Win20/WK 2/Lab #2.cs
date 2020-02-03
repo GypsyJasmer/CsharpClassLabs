90e6b24a3705f89ea6175bcf6e6116e70936e564
Sandi <59848573+GypsyJasmer@users.noreply.github.com>
2020-02-03 12:00:41 -0800

    adding first repo files

---
+++ C#Win20/WK 2/Lab #2.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/*
+6. 2nd Grader
+Input(nouns)
+	mathAnswer
+
+	
+Processing(verbs)
+	Generate random numbers
+	Generate random numbers
+	Display mathEquation
+	Get mathAnswer
+	Display sum
+
+	
+Output(nouns)
+	mathEquation
+	sum
+    */
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //define variable for answering the math question
+            int answer, sum, firstRand, secRand;
+
+            //generate random number
+            // creates an object called generator that can generate a random number
+            Random generator = new Random();
+            firstRand = generator.Next(1, 1000);
+
+            //gernerate random number
+            secRand = generator.Next(1, 1000);
+
+            //display math equestion
+            Console.WriteLine(String.Format("{0,5}", firstRand));
+            Console.WriteLine(String.Format("+{0,4}", secRand));
+
+            //get input of the math answer
+            Console.Write("What is the sum of the equation? ");
+            answer = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            //calculate the sum
+            sum = firstRand + secRand;
+
+            //display the sum of the equation
+            Console.WriteLine("The sum of the answer is:" +sum);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ C#Win20/WK 3/Lab3/Lab3/Program.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // define variable for answering the math question
+            const int ROCK=1, PAPER=2, SCISSORS = 3;
+            int uChoice, cChoice;
+
+            //display instructions
+            Console.WriteLine("We're going to play Rock, Paper, Scissors. " +
+                "You will be playing with a computer and entering 1 for rock, 2 for paper, or 3 for scissors." +
+                "Let's get going");
+
+            //get user choice
+            uChoice = int.Parse(Console.ReadLine());
+
+            //generate random computer choice
+            Random generator = new Random();
+            cChoice = generator.Next(1, 4);
+
+            //displaying what computer picked
+               if(cChoice==ROCK)
+            {
+                Console.WriteLine("Computer picked ROCK " +cChoice);
+            }
+            else if (cChoice==SCISSORS)
+            {
+                Console.WriteLine("Computer picked SCISSORS " +cChoice );
+            }
+            else
+            {
+                Console.WriteLine("Computer picked PAPER " + cChoice);
+            }
+            
+
+
+            //verify tie, win, or draw and display message. 
+            if (uChoice==cChoice)
+            {
+                Console.WriteLine("You tied with the computer");
+            }
+            else if(uChoice==ROCK && cChoice == SCISSORS ||
+                    uChoice==SCISSORS && cChoice==PAPER ||
+                    uChoice==PAPER && cChoice==ROCK)
+            {
+                Console.WriteLine("You won!!");
+            }
+            else
+            {
+                Console.WriteLine("Computer won!!");
+            }
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
